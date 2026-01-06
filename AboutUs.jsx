c287f874b9d8b2fa89a065122212069e098012a0
florencedominique <422002951@ntc.edu.ph>
2026-01-06 13:07:17 +0800

    AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,17 @@
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is dedicated to bringing nature closer to your home.
+        We offer a wide selection of healthy and affordable houseplants.
+      </p>
+      <p>
+        Our mission is to promote a greener lifestyle by making plants accessible
+        to everyone.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
